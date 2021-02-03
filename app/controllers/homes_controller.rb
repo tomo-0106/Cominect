a69be75287663d505bca1e3aeb9f798604ec5d5d
tomo-0106 <tomo0106.ran@gmail.com>
2021-02-03 12:34:45 +0000

    ランキング機能修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,23 @@
 class HomesController < ApplicationController
+
    def top
       @all_ranks = Comic.find(Review.group(:comic_id).order('count(comic_id) desc').limit(5).pluck(:comic_id))
+      review_ranks = {}
+      @all_ranks.each.with_index(1) do |comic, i|
+        review_ranks[comic.id] = comic.reviews.average(:rate).to_f.round(1)
+      end
+      sorted_review_ranks = review_ranks.sort_by { |_, v| v }.reverse.to_h
+      @ranks = []
+
+      sorted_review_ranks.each do |k,_|
+          @all_ranks.each do |rank|
+             if rank.id == k
+                @ranks << rank
+             end
+          end
+      end
+
+
       @all_rank = Comic.find(Favorite.group(:comic_id).order('count(comic_id) desc').limit(5).pluck(:comic_id))
    end
 
